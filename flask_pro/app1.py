645c3ff8dae1685c3f4e321b5e5858bde397b72a
Simran Grover <40205124+simrangrover5@users.noreply.github.com>
2020-07-13 18:36:50 +0530

    Update app1.py

--- flask_pro/app1.py
+++ flask_pro/app1.py
@@ -5,10 +5,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "<h1 style='color:red'>hi world</h1>"
+    return "<h1 style='color:red'>hello world</h1>"
 
 @app.route("/home")
 def home():
     return "hello to my home"
+
 
 app.run(debug=True)
